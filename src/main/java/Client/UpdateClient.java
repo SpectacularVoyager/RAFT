7110ec3045e9e84e32e664bc270e947b3ec3c54f
ankush <Mail2ankushd@gmail.com>
2025-04-25 22:52:09 +0530

    Made update client better

--- src/main/java/Client/UpdateClient.java
+++ src/main/java/Client/UpdateClient.java
@@ -1,14 +1,19 @@
 package Client;
 
+import Logging.AnsiColor;
+import Logging.RaftLogger;
 import RAFT.RPC.RPCServer;
 import RAFT.RPC.Server;
 import RAFT.RPC.ServerFactory;
+import RAFT.RPC.TCPSocket.ServerTCP;
 import RAFT.RPC.Type.RPCString;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class UpdateClient {
+    RaftLogger log = new RaftLogger();
 
     public static void main(String[] args) throws IOException {
         new UpdateClient();
@@ -17,11 +22,38 @@ public class UpdateClient {
     UpdateClient() throws IOException {
         RPCServer server = ServerFactory.getServer("tcp/localhost:8000", 1);
         Scanner in = new Scanner(System.in);
-        System.out.println("TYPE MESSAGE TO SEND TO:\t"+server);
-        while (in.hasNext()) {
-            System.out.println("raft> ");
+        log.log("TYPE MESSAGE TO SEND TO:\t" + server);
+        log.setFormat(AnsiColor.GREEN, 0, 0);
+        log.setFormat(AnsiColor.WHITE, 0, 0);
+        while (true) {
+            log.setFormat(AnsiColor.GREEN, 0, 0);
+            System.out.print("raft> ");
+            log.setFormat(AnsiColor.WHITE, 0, 0);
+
             String line = in.nextLine();
-            System.out.println(server.update(new RPCString(line)));
+            if (line.startsWith("changeaddr")) {
+                System.out.println(line);
+                server= ServerFactory.getServer(line.split(" ")[1], 1);
+                log.setFormat(AnsiColor.GREEN, 0, 0);
+                System.out.println("CHANGING SERVER ADDRESS TO:\t"+server);
+                continue;
+            }
+            var v = server.update(new RPCString(line));
+            if (v.isEmpty()) {
+                log.setFormat(AnsiColor.RED, 0, 0);
+                System.out.println("COULD NOT REACH SERVER");
+            } else if (v.get().isSuccess()) {
+                log.setFormat(AnsiColor.GREEN, 0, 0);
+                System.out.println(v);
+            } else {
+                log.setFormat(AnsiColor.YELLOW, 0, 0);
+                System.out.println(v);
+            }
         }
+    }
+
+
+    void setColor() {
+
     }
 }
